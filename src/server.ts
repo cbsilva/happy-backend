f6fb51943d2038b525df14bd1e8c5a4233208005
Cleberson <Silva>
2020-10-15 07:07:40 -0300

    criado classe para orfanato

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,47 @@
 import express from 'express';
-
+import { getRepository } from 'typeorm';
 import './database/connection';
+import Orphanage from './model/Orphanage';
 
 //cria aplicacao app
 const app = express();
 
 app.use(express.json());
 
+// por se tratar de um metodo que vai gravar no banco 
+// de dados é preciso usar o async na chamada
+// e await no metodo save
+app.post('/orphanages', async (request, response) => {
+    //recupera o conteudo enviado no body
+    const {
+        name,
+        latitude,
+        longitude,
+        about, 
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = request.body;
 
-app.post('/users/:id', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
+    // recebe os tipo de dados definidos na classe
+    const orphanagesRepository = getRepository(Orphanage);
+
+    // cria os dados (em memoria)
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about, 
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    // salva os dados no banco de dados
+    await orphanagesRepository.save(orphanage);
+
     return response.json({ message: 'hello world'});
 });
-
-
-
 
 
 //ouvindo a porta 3333
